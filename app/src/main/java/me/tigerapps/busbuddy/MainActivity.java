8ea91bdb41b424c755fc73d6f9527fd199d0fdef
Samuel Holland <samuel@sholland.org>
2017-10-15 01:20:10 -0500

    MainActivity: Add a minimal databinding-enabled layout

--- app/src/main/java/me/tigerapps/busbuddy/MainActivity.java
+++ app/src/main/java/me/tigerapps/busbuddy/MainActivity.java
@@ -1,14 +1,19 @@
 package me.tigerapps.busbuddy;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 
 import com.jakewharton.threetenabp.AndroidThreeTen;
+
+import me.tigerapps.busbuddy.databinding.MainActivityBinding;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidThreeTen.init(getApplication().getBaseContext());
+        final MainActivityBinding binding =
+                DataBindingUtil.setContentView(this, R.layout.main_activity);
     }
 }
