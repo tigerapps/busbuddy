5369f769f86976676b4f249f35d0b29c37cf3ee5
Samuel Holland <samuel@sholland.org>
2017-10-15 00:43:27 -0500

    app: Add library for java.time backport

--- app/src/main/java/me/tigerapps/busbuddy/MainActivity.java
+++ app/src/main/java/me/tigerapps/busbuddy/MainActivity.java
@@ -3,9 +3,12 @@ package me.tigerapps.busbuddy;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.jakewharton.threetenabp.AndroidThreeTen;
+
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AndroidThreeTen.init(getApplication().getBaseContext());
     }
 }
